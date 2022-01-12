362f4f07a05f223fdc30923459aef5dc3fca14b5
standup8 <borapark13@gmail.com>
2022-01-12 23:13:05 +0900

    Modify: Update common style of wrapper in theme.tsx

--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -7,9 +7,11 @@ export const theme: ThemeProps = {
   mainColor: "#333333",
   fontBlack: "#000000",
   fontWhite: "#ffffff",
+
   wrapper: {
     width: "100%",
     maxWidth: "600px",
     margin: "0 auto",
+    backgroundColor: "#ffffff",
   },
 };
