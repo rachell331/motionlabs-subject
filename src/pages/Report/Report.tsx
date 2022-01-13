33458a8da655df81b32c0d328887c37e103c66db
standup8 <borapark13@gmail.com>
2022-01-13 10:31:08 +0900

    Add: BarChart.tsx & Modify Unnecessary variables.

--- src/pages/Report/Report.tsx
+++ src/pages/Report/Report.tsx
@@ -1,7 +1,88 @@
-import React, { FC } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import { BarChart } from "./BarChart";
+import { LineChart } from "./LineChart";
 
-interface Props {}
+export interface UserDataType {
+  startDate: string;
+  endDate: string;
+  period: number;
+  cycle: number;
+}
+export const Report = () => {
+  const [userData, setUserData] = useState<UserDataType[]>([
+    { startDate: "", endDate: "", period: 0, cycle: 0 },
+  ]);
+  useEffect(() => {
+    axios
+      .get("https://motionz-kr.github.io/playground/apis/report.json")
+      .then((res) => {
+        setUserData(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-export const Report: FC = (props: Props) => {
-  return <div></div>;
+  return (
+    <Container>
+      <Title>User Report</Title>
+      <ChartWrapper>
+        <IndexBox>
+          <Cycle></Cycle>
+          <Unit>활동 주기</Unit>
+          <Period></Period>
+          <Unit>활동 기간, 시작일</Unit>
+        </IndexBox>
+        <LineChart userData={userData} />
+        <BarChart userData={userData} />
+      </ChartWrapper>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  ${({ theme }) => theme.wrapper};
+  height: 100vh;
+  padding: 20px;
+  overflow: hidden;
+`;
+
+const Title = styled.h2`
+  padding: 20px;
+  font-size: 1.5rem;
+  font-weight: bold;
+`;
+
+const ChartWrapper = styled.div`
+  width: 560px;
+  height: 480px;
+  border: 1px solid #eaeaea;
+  border-radius: 10px;
+`;
+
+const IndexBox = styled.div`
+  padding: 10px;
+  text-align: right;
+`;
+const Cycle = styled.span`
+  display: inline-block;
+  width: 8px;
+  height: 8px;
+  margin-right: 5px;
+  background-color: ${({ theme }) => theme.mainColor};
+  border-radius: 50%;
+`;
+const Unit = styled.span`
+  font-size: 0.65rem;
+  font-weight: 100;
+`;
+const Period = styled.span`
+  display: inline-block;
+  width: 24px;
+  height: 8px;
+  margin: 0 5px 0 15px;
+  background-color: ${({ theme }) => theme.mainColor};
+  border-radius: 4px;
+`;
--- src/pages/Report/components/Bar.tsx
+++ src/pages/Report/components/Bar.tsx
@@ -1,7 +1,49 @@
-import React, { FC } from "react";
+import React from "react";
+import styled from "styled-components";
+import { UserDataType } from "../Report";
 
-interface Props {}
+interface UserProps {
+  user: UserDataType;
+  index: number;
+}
 
-export const Bar: FC = (props: Props) => {
-  return <div></div>;
+export const Bar = ({ user, index }: UserProps) => {
+  const changeDateFormat = (date: string) => {
+    const month: string = date.slice(-5, -3);
+    const day: string = date.slice(-2);
+    const newDateString: string = month + "/" + day;
+    return newDateString;
+  };
+
+  return (
+    <Container index={index}>
+      <Period>{user.period}일</Period>
+      <Rectangle period={user.period} />
+      <StartDate>{changeDateFormat(user.startDate)}</StartDate>
+    </Container>
+  );
 };
+
+const Container = styled.div<{ index: number }>`
+  position: absolute;
+  left: ${(props) => `calc(100%/6 * (${props.index} + 1))`};
+  bottom: 0;
+  font-size: 0.67rem;
+  font-weight: 600;
+`;
+
+const Period = styled.p`
+  margin-bottom: 4px;
+  text-align: center;
+`;
+
+const Rectangle = styled.div<{ period: number }>`
+  width: 30px;
+  height: ${(props) => `calc(${props.period} * 10px)`};
+  background-color: ${({ theme }) => theme.mainColor};
+  border-radius: 8px;
+`;
+
+const StartDate = styled.p`
+  margin: 4px 0 20px;
+`;
